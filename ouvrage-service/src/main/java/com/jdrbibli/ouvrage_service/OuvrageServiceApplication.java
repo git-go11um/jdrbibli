29540aedfe542d8ef4ed0043a5e1c6c704a3a2bf
go11um <v.haumont@gmail.com>
2025-06-27 13:38:08 +0200

    commit first commit

---
+++ ouvrage-service/src/main/java/com/jdrbibli/ouvrage_service/OuvrageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jdrbibli.ouvrage_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuvrageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuvrageServiceApplication.class, args);
+	}
+
+}
